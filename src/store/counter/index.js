b4483e243e22beccb8c9e924b61d0f624815a336
Marina8989 <marina.mill209@gmail.com>
2021-12-21 13:59:13 -0800

    redux

--- src/store/counter/index.js
+++ src/store/counter/index.js
@@ -3,13 +3,14 @@ export const INCREMENT_COUNTER = 'INCREMENT_COUNTER';
 export const DECREMENT_COUNTER = 'DECREMENT_COUNTER';
 
 function counterReducer(state=initialState, action){
-    if(action.type === INCREMENT_COUNTER){
-       return state + 1;
-    }
-    if(action.type === DECREMENT_COUNTER) {
-       return state- 1;
-    }
-     return state;
+   switch(action.type){
+      case INCREMENT_COUNTER:
+        return state + 1;
+      case DECREMENT_COUNTER:
+        return state - 1;
+      default: 
+        return state;  
+   }
 }
 
 export default counterReducer;
