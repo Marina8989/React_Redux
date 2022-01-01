b3cb07d7012cd0920d036daa2365910dfacfc500
Marina8989 <marina.mill209@gmail.com>
2022-01-01 14:51:01 -0800

    react redux

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import { ToastContainer } from "react-toastify";
+import { Switch, Route } from "react-router-dom";
+import Navbar from './components/Navbar';
+import Home from './components/Home';
 
 const App = () => {
     return (
         <div>
           <ToastContainer />
-          <h1>Welcome to React Redux Contact Book</h1>
+          <Navbar />
+          <Switch>
+              <Route exact path="/" >
+                <Home />
+              </Route>
+              <Route path="/add" >
+                <h1>add</h1>
+              </Route>
+              <Route path="/edit/:id" >
+                <h1>edit</h1>
+              </Route>
+           </Switch>
         </div>
     )
 }
