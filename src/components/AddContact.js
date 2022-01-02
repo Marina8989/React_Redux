2fdb975eb67ce3e0b5984b7eb3e39ef2e138e380
Marina8989 <marina.mill209@gmail.com>
2022-01-01 18:10:58 -0800

    redux

--- src/components/AddContact.js
+++ src/components/AddContact.js
@@ -1,20 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import { toast } from "react-toastify";
 
 const AddContact = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [number, setNumber] = useState("");
+  const contacts = useSelector(state => state);
+  console.log(contacts)
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const checkEmail = contacts.find(contact => contact.email === email && contact)
+    const checkNumber = contacts.find(contact => contact.number === parseInt(number))
+    const checkName = contacts.find(contact => contact.name === name && contact)
+    
+     if(!name || !email || !number) {
+         return toast.warning("Please fill in all fields");
+     }
+     if(checkEmail) {
+       return toast.error("This email already exists!");
+     }
+     if(checkName) {
+       return toast.error("This name already exists!");
+     }
+     if(checkNumber) {
+       return toast.error("This number already exists!");
+     }
+
+     const data = {
+       id: contacts[contacts.length - 1].id + 1,
+       name,
+       email,
+       number
+     }
+
+     console.log('data', data)
+  }
   return (
     <div className="container">
       <h1 className="display-3 my-5 text-center">Add Student</h1>
       <div className="row">
         <div className="col-md-6 shadow mx-auto p-5">
-          <form>
+          <form onSubmit={handleSubmit}>
             <div className="form-group">
-              <input type="text" placeholder="Name" className="form-control" />
+              <input type="text" placeholder="Name" className="form-control" value={name} onChange={e => setName(e.target.value)} />
             </div>
             <div className="form-group">
               <input
                 type="email"
                 placeholder="Email"
                 className="form-control"
+                value={email}
+                onChange={e => setEmail(e.target.value)}
               />
             </div>
             <div className="form-group">
@@ -22,6 +61,8 @@ const AddContact = () => {
                 type="number"
                 placeholder="Phone number"
                 className="form-control"
+                value={number}
+                onChange={e=> setNumber(e.target.value)}
               />
             </div>
             <div className="form-group">
--- src/redux/reducers/contactReducer.js
+++ src/redux/reducers/contactReducer.js
@@ -2,17 +2,22 @@ const initialState = [
     {
        id: 0,
        name: "Raman Sharma",
-       number: 123456789
+       number: 123456789,
+       email: "raman@gmail.com"
     },
     {
        id: 1,
        name: "test name",
-       number: 987654321
+       number: 1987654321,
+       email: "test@gmail.com"
     }
 ]
 
+
 const contactReducer = ( state = initialState, action) => {
    switch(action.type){
+       case "ADD_CONTACT":
+          return [...state, action.payload];
         default:
           return state;
    }
