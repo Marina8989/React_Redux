fe9e24d338424c201b5d8c10032b1eb39f99efdc
Marina8989 <marina.mill209@gmail.com>
2022-01-07 11:32:03 -0800

    redux

--- src/App.js
+++ src/App.js
@@ -1,37 +1,13 @@
-import React, { useEffect } from "react";
-import { connect } from "react-redux";
-import { getData, removeUser } from  "./store/users/actions"; 
-//import { deletedUser } from "./store/deletedUsers/actions";
+import React from "react";
+import Navbar from "./components/Navbar";
 
-function App(props){
-  console.log(props.users)
-  useEffect(()=> {
-     props.getData();
-  }, [])
-  return(
-      <div>
-         <h3>List of Names</h3>
-         {props.isLoading && <h3>Loading...</h3>}
-          <div>
-           {props.users.map(user => <div key={user.id} onClick={() => props.removeUser(user)}>{user.name}</div>)}
-         </div>
-         <br />
-         <div>  
-           {props.deletedUsers.map(user => <div key={user.name}>{user.name}</div>)}
-          </div>
-      </div>
-  )
+function App(){
+    return (
+        <div style={{margin: "0 auto", textAlign: "center"}}>
+            <Navbar />
+            <h2>eCommerce Shop</h2>
+        </div>
+    )
 }
 
-const mapStateToProps = (state) => ({
-   users: state.users.data,
-   isLoading: state.users.isLoading,
-   isError: state.users.isError,
-   deletedUsers: state.deletedUsers.data
-})
-const mapDispatchToProps = {
-  getData,
-  removeUser
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default App;
--- src/store/deletedUsers/actions.js
+++
@@ -1,8 +0,0 @@
-import { DELETED_USER } from "./index";
-
-export const deletedUser = (user) => (dispatch, getState) => {
-    dispatch({
-        type: DELETED_USER,
-        payload: user
-    })
-}
